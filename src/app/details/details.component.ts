962a90157a2268575d57bd6601fe47bf2a2dc33f
danielp <danny.pretorius.developer@gmail.com>
2024-08-31 15:22:19 +0200

    Routing to details page now implemented

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
-  templateUrl: './details.component.html',
+  imports: [CommonModule],
+  template: `
+  <p>Detail Works {{ this.housingLocationId}}</p>
+  `,
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
 
+  constructor(){
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
