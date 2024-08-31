177f468c0cd622f0aaad7740d5d4fa0e83a1f0f6
danielp <danny.pretorius.developer@gmail.com>
2024-08-31 13:45:42 +0200

    First commit after adding the router

---
+++ src/app/routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component'; 
+
+const routeConfig: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home page',
+      }
+];
+
+
+export default routeConfig;
